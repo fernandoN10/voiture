9eec063e16b32c575280617f7c646feb412e7e31
fernandoN10 <152297932+fernandoN10@users.noreply.github.com>
2024-01-30 19:42:24 +0100

    Delete home.py

--- home.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-def app():
-    df = pd.read_csv('https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv').head(10)
-    st.markdown( """<h1 style="text-align: center;">ANALYSES DU DATASET DES VOITURES</h1>""", unsafe_allow_html=True,)
-    st.write('')
-    st.write('Voici les 10 premiers lignes')
-    st.dataframe(df, use_container_width=True)
